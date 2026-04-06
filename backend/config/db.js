e34a89f9d94598eb83ce0515123206646f2e847e
unknown <preetiyadavpanihar@gmail.com>
2026-04-06 17:16:33 +0530

    Removed hardcoded MongoDB URI and moved to env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://preetiyadavpanihar:Pa9350251613@cluster0.ijnmyqj.mongodb.net/Food-delivery').then(()=>console.log("db connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("db connected"));
 }
