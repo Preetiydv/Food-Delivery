ca280efe6dff24826ddbe2b8cafb294e82861f5d
unknown <preetiyadavpanihar@gmail.com>
2026-04-08 17:17:48 +0530

    Implemented user authentication with login and registration using JWT, bcrypt, and input validation

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 
 
@@ -21,6 +23,7 @@ connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 
 app.get("/",(req,res)=>{
