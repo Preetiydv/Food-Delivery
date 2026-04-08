96d5f1336d358ca9ec89c7de2e35cdd39973fae8
unknown <preetiyadavpanihar@gmail.com>
2026-04-08 21:29:11 +0530

    integrated authentication with UI ,improved UX , added cart protection

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,25 +1,66 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../Context/StoreContext'
+import axios from "axios"
 
 const LoginPopup = ({setShowLogin}) => {
+
+    const {url,setToken} = useContext(StoreContext)
+
     const [currState,setCurrState]=useState("Login");
+    
+    const [data,setData] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+    }
+
+    const onLogin = async (event) => {
+        event.preventDefault()
+        let newUrl = url;
+        if(currState==="Login"){
+            newUrl += "/api/user/login"
+        }
+        else{
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl,data);
+    
+        if(response.data.token){
+            setToken(response.data.token);
+            localStorage.setItem("token",response.data.token);
+            setShowLogin(false)
+        }
+        else{
+            alert(response.data.message)
+        }
+
+    }
+
     const[showPassword,setShowPassword]=useState(false);
   return (
     <div className='login-popup' onClick={()=>setShowLogin(false)}>
-        <form  className="login-popup-container" onClick={(e)=>e.stopPropagation()}>
+        <form onSubmit={onLogin} className="login-popup-container" onClick={(e)=>e.stopPropagation()}>
             <div className="login-popup-title">
                 <h2>{currState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
 
             </div>
             <div className="login-popup-inputs">
-                {currState==="Login"?<></>:<input type="text" placeholder='Your Name' required />}
+                {currState==="Login"?<></>:<input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your Name' required />}
                 
-                <input type="email" placeholder='Your email' required />
+                <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required />
                 
                 <div className="password-field">
-                <input 
+                <input name='password' onChange={onChangeHandler} value={data.password}
                 type={showPassword ? "text" : "password"} 
                 placeholder='Password' 
                 required 
@@ -28,7 +69,7 @@ const LoginPopup = ({setShowLogin}) => {
                 </span>
                 </div>
             </div>
-            <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+            <button type='submit'>{currState==="Sign Up"?"Create Account":"Login"}</button>
             <div className="login-popup-condition">
                 <input type="checkbox" required/>
                 <p>By continuing, i agree to the terms of use & privacy policy.</p>
