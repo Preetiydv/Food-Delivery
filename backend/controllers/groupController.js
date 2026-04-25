159e211e334e73524939578eb4cc1d7c9b8b62e3
unknown <preetiyadavpanihar@gmail.com>
2026-04-25 21:37:21 +0530

    Improved group order flow with host displau and checkoutintegration

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -1,127 +1,129 @@
 import groupModel from "../models/groupModel.js";
+import userModel from "../models/userModel.js";
 import { v4 as uuidv4 } from "uuid";
 
 //Create group
-const createGroup = async (req,res) => {
-    try {
-        const groupId = uuidv4();
+const createGroup = async (req, res) => {
+  try {
+    const groupId = uuidv4();
 
-        const group = new groupModel({
-            groupId,
-            host: req.userId,
-            members: [req.userId],
-            items: []
-        });
-        await group.save();
+    const user = await userModel.findById(req.userId);
 
-        res.json({success:true,groupId});
+    const group = new groupModel({
+      groupId,
+      host: user.name,
+      members: [req.userId],
+      items: [],
+    });
+    await group.save();
 
-    } catch (error) {
-        console.log(error);
-        res.json({success:false});
-    }
+    res.json({ success: true, groupId });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
 };
 
 //join group
-const joinGroup = async (req,res) => {
-    const {groupId} = req.body;
-    try {
-        const group = await groupModel.findOne({groupId});
+const joinGroup = async (req, res) => {
+  const { groupId } = req.body;
+  try {
+    const group = await groupModel.findOne({ groupId });
 
-        if(!group){
-            return res.json({success:false,message:"Group not found"});
-        }
-
-        if(!group.members.includes(req.userId)){
-            group.members.push(req.userId);
-            await group.save();
-        }
-
-        res.json({success:true, group});
-    } catch (error) {
-        console.log(error);
-        res.json({success:false});
+    if (!group) {
+      return res.json({ success: false, message: "Group not found" });
     }
+
+    if (!group.members.includes(req.userId)) {
+      group.members.push(req.userId);
+      await group.save();
+    }
+
+    res.json({ success: true, group });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
 };
 
 const getGroup = async (req, res) => {
-    try {
-        const { groupId } = req.body;
+  try {
+    const { groupId } = req.body;
 
-        const group = await groupModel.findOne({ groupId });
+    const group = await groupModel.findOne({ groupId });
 
-        if (!group) {
-            return res.json({
-                success: false,
-                message: "Group not found"
-            });
-        }
-
-        res.json({
-            success: true,
-            group
-        });
-
-    } catch (error) {
-        console.log(error);
-        res.json({
-            success: false,
-            message: "Error"
-        });
+    if (!group) {
+      return res.json({
+        success: false,
+        message: "Group not found",
+      });
     }
-}
+
+    res.json({
+      success: true,
+      group,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 
 const addItemToGroup = async (req, res) => {
-    try {
-        const { groupId, item } = req.body;
+  try {
+    const { groupId, item } = req.body;
 
-        const group = await groupModel.findOne({ groupId });
+    const group = await groupModel.findOne({ groupId });
 
-        if (!group) {
-            return res.json({
-                success: false,
-                message: "Group not found"
-            });
-        }
-
-        group.items.push({
-            userId: req.userId,
-            itemId: item._id,
-            name: item.name,
-            quantity: 1,
-            price: item.price
-        });
-
-        await group.save();
-
-        res.json({
-            success: true,
-            message: "Item added to group cart",
-            group
-        });
-
-    } catch (error) {
-        console.log(error);
-        res.json({success: false,message: "Error"});
+    if (!group) {
+      return res.json({
+        success: false,
+        message: "Group not found",
+      });
     }
+
+    group.items.push({
+      userId: req.userId,
+      itemId: item._id,
+      name: item.name,
+      quantity: 1,
+      price: item.price,
+    });
+
+    await group.save();
+
+    res.json({
+      success: true,
+      message: "Item added to group cart",
+      group,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
-const placeGroupOrder = async (req,res) => {
-    try {
-        const {groupId} = req.body;
-        const group = await groupModel.findOne({groupId});
-        if(!group){
-            return res.json({success:false,message:'Group not found'});
-
-        }
-        group.status = "Completed";
-        await group.save();
-        res.json({success:true,message:"Group Order placed successfully",group});
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:"Error placing group order"});
+const placeGroupOrder = async (req, res) => {
+  try {
+    const { groupId } = req.body;
+    const group = await groupModel.findOne({ groupId });
+    if (!group) {
+      return res.json({ success: false, message: "Group not found" });
     }
+    group.status = "Completed";
+    await group.save();
+    res.json({
+      success: true,
+      message: "Group Order placed successfully",
+      group,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error placing group order" });
+  }
 };
 
-
-export { createGroup, joinGroup, getGroup,addItemToGroup, placeGroupOrder };
+export { createGroup, joinGroup, getGroup, addItemToGroup, placeGroupOrder };
